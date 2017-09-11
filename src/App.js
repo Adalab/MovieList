87ff989c428400573941330574f38add68fbcf22
Elena Mateos Lopez <elena.mateos1@gmail.com>
2017-09-11 19:26:53 +0200

    Added function to change the status in app by the toggleFav

--- src/App.js
+++ src/App.js
@@ -10,18 +10,28 @@ class App extends Component {
         img : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png",
         fav : false,
         title : "Marta es la mejor",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 1
       },
       {
         img : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png",
         fav : false,
         title : "Star Wars",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
+        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 2
       }
     ]
     }
   }
+toggleFav(id){
+  const newMovies = this.state.movies.clone();
+  newMovies.map(movie => {
+    if (id === movie.id) {
+      movie.fav = !movie.fav;
+    }
+  })
+  this.setState({movies: newMovies});
+}
 
   render() {
     return (
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 class MovieCard extends React.Component {
+  handleClick(){
+    
+  }
   render () {
     return (
       <div className="card">
         <img className="img-card" width="200px" src={this.props.img}/>
-        <img className="favStar" width="50px" src={this.props.fav?"http://pngimg.com/uploads/star/star_PNG1589.png":"http://pngimg.com/uploads/star/star_PNG1595.png"} />
+        <img className="favStar" onClick={this.handleClick} width="50px" src={this.props.fav?"http://pngimg.com/uploads/star/star_PNG1589.png":"http://pngimg.com/uploads/star/star_PNG1595.png"} />
         <h1>{this.props.title}</h1>
         <p>{this.props.description}</p>
       </div>
